9ad5e7da5a87b363c78f04fe590612a647db52c5
Oleg Rybin <30134150+NuMB2014@users.noreply.github.com>
2021-05-17 17:45:35 +0700

    Update 1 задание.sql

--- 1 задание.sql
+++ 1 задание.sql
@@ -8,4 +8,4 @@ WHERE
     community_members.user_id = users.id
         AND communities.id = community_members.community_id
         AND communities.created_at >= '2013-01-01 00:00:00'
-ORDER BY community_members.joined_at;
+ORDER BY community_members.joined_at DESC;
